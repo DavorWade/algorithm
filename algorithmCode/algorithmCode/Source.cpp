32e22a7d683fdc6b00d8e97a13021ff2592aa5cf
Davor <daviswade@sina.com>
2016-05-14 00:38:30 +0800

    init

--- algorithmCode/algorithmCode/Source.cpp
+++ algorithmCode/algorithmCode/Source.cpp
@@ -3,7 +3,9 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-	system("pause");
+
+int main(int argc, char *argv[]) {
+
+	//system("pause");
+	return 0;
 }
--- algorithmCode/algorithmCode/practice.cpp
+++ algorithmCode/algorithmCode/practice.cpp
@@ -1,9 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stack>
 
 using namespace std;
 
+/** 结点的数据*/
+typedef int tree_node_elem_t;
+/*
+*@struct
+*@brief 二叉树结点
+*/
+struct binary_tree_node_t {
+	binary_tree_node_t* left;
+	binary_tree_node_t* right;
+	tree_node_elem_t elem;
+};
+
+void preorder(const binary_tree_node_t* root, int(*visit)(const binary_tree_node_t*)) {
+	const binary_tree_node_t* tem = root;
+	stack<const binary_tree_node_t*> s;
+
+	if (tem != nullptr) s.push(tem);
+
+	while (!s.empty()) {
+		tem = s.top();
+		s.pop();
+		visit(tem);
+
+		if (tem->right != nullptr) s.push(tem->right);
+		if (tem->left != nullptr) s.push(tem->left);
+	}
+}
+
+void inorder(const binary_tree_node_t* root, int(*visit)(const binary_tree_node_t*)) {
+	const binary_tree_node_t* p = root;
+	stack<const binary_tree_node_t*> s;
+
+	/*if (p != nullptr) s.push(p);
+
+	while (!s.empty()) {
+		p = s.top();
+		s.pop();
+
+		if (p->left != nullptr) {
+			s.push(p);
+			s.push(p->left);
+		}
+		else {
+			visit(p);
+			if (p->right != nullptr)
+				s.push(p->right);
+		}
+	}*/
+	while (!s.empty() || p != nullptr) {
+		if (p != nullptr) {
+			s.push(p);
+			p = p->left;
+		}
+		else {
+			p = s.top();
+			s.pop();
+			visit(p);
+
+			p = p->right;
+		}
+	}
+}
+
 int main(int argc, char *argv[])
 {
-	system("pause");
+	//printf("%d\n", (1 << n) - 1); /* 总次数*/
+
+	return 0;
 }
+
