b3753d6c8bd7dd81a17393edac58ac801e96cc7f
Davor <daviswade@sina.com>
2016-05-20 00:06:59 +0800

    init

--- LintCode/LintCode/practice.cpp
+++ LintCode/LintCode/practice.cpp
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stack>
+#include <queue>
 
 using namespace std;
 
-void change(int n, const int d) {
-	stack<int> s;
-	int tem;
-
-	while (n != 0) {
-		tem = n % d;
-		n = n / d;
-		s.push(tem);
-	}
-
-	while (!s.empty()) {
-		tem = s.top();
-		s.pop();
-		printf("%d", tem);
+void yanghui_triangle(const int n) {
+	queue<int> q;
+	/* 预先放入第一行的1 */
+	q.push(1);
+	for (int i = 0; i <= n; i++) { /* 逐行处理*/
+		int s = 0;
+		q.push(s); /* 在各行间插入一个0*/
+		/* 处理第i 行的i+2 个系数（包括一个0）*/
+		for (int j = 0; j < i + 2; j++) {
+			int t;
+			int tmp;
+			t = q.front(); /* 读取一个系数，放入t*/
+			q.pop();
+			tmp = s + t; /* 计算下一行系数，并进队列*/
+			q.push(tmp);
+			s = t; /* 打印一个系数，第i+2 个是0*/
+			if (j != i + 1) {
+				printf("%d ", s);
+			}
+		}
+		printf("\n");
 	}
 }
 
-
 int main(int argc, char *argv[]) {
-	change(7, 4);
 	printf("\n");
 
 	system("pause");
