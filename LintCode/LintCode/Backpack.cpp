63de8d6bca9ccef5532097132869bacb6c14b8c5
gitNewer <gitNewer@163.com>
2016-06-09 15:13:26 +0800

    init

---
+++ LintCode/LintCode/Backpack.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	/**
+	* http://www.lintcode.com/en/problem/backpack/
+	* @param m: An integer m denotes the size of a backpack
+	* @param A: Given n items with size A[i]
+	* @return: The maximum size
+	*/
+	int backPack(int m, vector<int> A) {
+		// write your code here
+		if (A.empty() || m < 1) return 0;
+
+		const int N = A.size() + 1;
+		const int M = m + 1;
+		vector<vector<bool> > dp(N, vector<bool>(M, false));
+
+		dp[0][0] = true;
+		for (int i = 1; i != N; ++i) {
+			for (int j = 0; j != M; ++j) {
+				if (j < A[i - 1]) {
+					dp[i][j] = dp[i - 1][j];
+				}
+				else {
+					dp[i][j] = dp[i - 1][j] || dp[i - 1][j - A[i - 1]];
+				}
+			}
+		}
+
+		// return the largest i if true
+		for (int i = M; i > 0; --i) {
+			if (dp[N - 1][i - 1]) {
+				return (i - 1);
+			}
+		}
+		return 0;
+	}
+};
