6938426bb67a82dc86fc1b1f23b6e9e8f0c833bf
DavorWade <daviswade@sina.com>
2016-07-05 17:33:54 +0800

    poj1007

---
+++ POJ/POJ/poj1007.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <utility>
+#include <set>
+
+using namespace std;
+
+/*
+int main()
+{
+	int n, m;
+	int count;
+	set<pair<int, int> > rs;
+	set<pair<int, int> >::iterator it;
+
+	cin >> n >> m;
+
+	char **data = new char*[m];
+	for (int i = 0; i < m; i++)
+	{
+		data[i] = new char[n + 1];
+	}
+
+	for (int j = 0; j < m; j++)
+	{
+		//std::cin.getline(data[j], n+1);
+		cin >> data[j];
+	}
+	for (int k = 0; k < m; k++)
+	{
+		count = 0;
+		for (int u = 0; u < n; u++)
+		{
+			for (int t = u + 1; t < n; t++)
+			{
+				if (data[k][u]>data[k][t])
+				{
+					count++;
+				}
+			}
+		}
+		rs.insert(make_pair(count, k));
+	}
+
+	for (it = rs.begin(); it != rs.end(); it++)
+	{
+		cout << data[(*it).second] << endl;
+	}
+	for (int ptr = 0; ptr < m; ptr++)
+	{
+		delete[] data[ptr];
+	}
+	delete[] data;
+	return  0;
+}
+*/
