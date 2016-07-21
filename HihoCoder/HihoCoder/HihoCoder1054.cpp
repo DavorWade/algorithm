b10317815ee832502b179e265cb91229a1e60a27
DavorWade <daviswade@sina.com>
2016-07-21 17:20:05 +0800

    HihoCoder1054

--- HihoCoder/HihoCoder/HihoCoder1054.cpp
+++ HihoCoder/HihoCoder/HihoCoder1054.cpp
@@ -1,76 +1,73 @@
-#include <iostream>
-#include <cstring>
-#include <vector>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <algorithm>
+
 
 using namespace std;
 
-int cnt, aim_depth;
-int vis[10];
-int record[10][10];
-int map[10][10];
-int total_edge;
+int rs; // total legal solution
+int totalEdgeCount; //total edge count in every input data
+int record[10][10]; //store illegal edge
+int data[10][10]; //store edge from input data 
+int pointCount; //total point count in result
+int vis[10]; // is visited
 
-void init()
-{
+void init() {
 	record[1][3] = record[3][1] = record[1][7] = record[7][1] = 1;
 	record[1][9] = record[9][1] = record[3][7] = record[7][3] = 1;
 	record[9][3] = record[3][9] = record[9][7] = record[7][9] = 1;
 	record[2][8] = record[8][2] = record[4][6] = record[6][4] = 1;
 }
 
-void dfs(int num, int depth, int edge)
-{
-	//找到符合条件的案例是深度，其次我们找到的边要包括已看到的边 
-	if (depth == aim_depth && edge == total_edge)
-	{
-		cnt++;
+void depSearch(int leftPoint, int currPointCount, int currEdgeCount) {
+	if (currPointCount == pointCount && currEdgeCount == totalEdgeCount) {
+		rs++;
 		return;
 	}
 
-	for (int i = 1; i < 10; i++)
-	{
-		if (!vis[i])
-		{
-			if (record[num][i] && !vis[(num + i) / 2]) continue;  //中间点划过，则不找 
+	for (int i = 1; i < 10; i++) {
+		if (!vis[i]) {
+			if (record[leftPoint][i] && !vis[(leftPoint + i) / 2]) continue;
 			vis[i] = 1;
-			if (map[num][i])   //看看是否是已存在的边 
-				dfs(i, depth + 1, edge + 1);
-			else
-				dfs(i, depth + 1, edge);
+			if (data[leftPoint][i]) 
+				depSearch(i, currPointCount + 1, currEdgeCount + 1);
+			else 
+				depSearch(i, currPointCount + 1, currEdgeCount);
 			vis[i] = 0;
 		}
 	}
 }
 
-int main()
-{
-	int t, n, uu, vv;
-	cin >> t;
+int main() {
+	int n, m, a, b;
+	scanf("%d", &n);
+
 	init();
-	while (t--)
-	{
-		cin >> n;
+	while (n > 0) {
+		scanf("%d", &m);
 
 		memset(vis, 0, sizeof(vis));
-		memset(map, 0, sizeof(map));
+		memset(data, 0, sizeof(data));
 
-		for (int i = 0; i < n; i++)
-		{
-			cin >> uu >> vv;
-			map[uu][vv] = map[vv][uu] = 1;
+		for (int i = 0; i < m; i++) {
+			scanf("%d %d", &a, &b);
+			data[a][b] = data[b][a] = 1;
 		}
-		cnt = 0;
-		total_edge = n;
-		for (aim_depth = max(4, n + 1); aim_depth <= 9; aim_depth++)
-		{
-			for (int i = 1; i <= 9; i++)
-			{
+
+		rs = 0;
+		totalEdgeCount = m;
+		for (pointCount = max(4, m + 1); pointCount < 10; pointCount++) {
+			for (int i = 1; i < 10; i++) {
 				vis[i] = 1;
-				dfs(i, 1, 0);
+				depSearch(i, 1, 0);
 				vis[i] = 0;
 			}
 		}
-		cout << cnt << endl;
+
+		printf("%d\n", rs);
+		n--;
 	}
+
+	return 0;
 }
