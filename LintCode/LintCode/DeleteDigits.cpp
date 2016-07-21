95ace429c7068da5bfc727d0eecf27d5d6b6e25d
DavorWade <daviswade@sina.com>
2016-07-21 22:15:25 +0800

    Delete Digits

--- LintCode/LintCode/DeleteDigits.cpp
+++ LintCode/LintCode/DeleteDigits.cpp
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <vector>
+#include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -10,14 +12,42 @@ public:
 	*@param A: A positive integer which has N digits, A is a string.
 	*@param k: Remove k digits.
 	*@return: A string
+	*@date: 2016-7-21
 	*/
 	string DeleteDigits(string A, int k) {
 		// wirte your code here
+		int count = A.size() - k;
+		int start = 0;
+		int startIndex = 0;
+		int end = k;
+		string rs = "";
 
+		while (count > 0) {
+			char tem = A[start];
+			startIndex = start; // if testcase is increase like "123456", below code 'startIndex = start;' will never be excuted.
+			for (; start <= end && end < A.size(); start++) {
+				if (A[start] < tem) {
+					tem = A[start];
+					startIndex = start;
+				}
+			}
+
+			rs += tem;
+			start = startIndex + 1;
+			end++;
+			count--;
+		}
+
+		rs.erase(0, rs.find_first_not_of("0")); //remove the charactor '0' in the front of rs.
+		return rs;
 	}
 };
 
 int main(int argc, char *argv[]){
+	Solution s;
+	string rs = s.DeleteDigits("123456789", 2);
+
+	cout << "rs:" << rs << endl;
 
 	system("pause");
 	return 0;
--- LintCode/LintCode/practice.cpp
+++ LintCode/LintCode/practice.cpp
@@ -9,7 +9,6 @@
 #include <sstream>
 #include <locale>
 
-
 using namespace std;
 
 void yanghui_triangle(const int n) {
@@ -86,81 +85,6 @@ public:
 	}
 };
 
-class Sentence {
-private:
-	vector<string> &split(const string &s, char delim, vector<string> &elems) {
-		stringstream ss(s);
-		string item;
-		while (getline(ss, item, delim)) {
-			elems.push_back(item);
-		}
-		return elems;
-	}
-
-	vector<string> split(const string &s, char delim) {
-		vector<string> elems;
-		split(s, delim, elems);
-		return elems;
-	}
-
-public:
-	string process(const string s) {
-		string originSen;
-		bool isCapital = false;
-		vector<string> rs;
-		char delim = ' ';
-
-		rs = split(s, delim);
-
-		for (int i = 0; i < rs.size(); i++) {
-			if (rs[i] != "") {
-				if (rs[i] == ","){
-					originSen.append(",");
-				}
-				else if (rs[i] == "."){
-					originSen.append(". ");
-					isCapital = true;
-				}
-				else {
-					transform(rs[i].begin(), rs[i].end(), rs[i].begin(), ::tolower);
-					if (isCapital || i == 0) {
-						locale loc;
-						rs[i][0] = toupper(rs[i][0], loc);
-						originSen.append(rs[i]);
-					}
-					else {
-						originSen.append(" ");
-						originSen.append(rs[i]);
-					}
-				}
-			}
-		}
-
-		return originSen;
-	}
-
-};
-
-
-int main() {
-	vector<string> data;
-	string tem;
-	Sentence s;
-
-	getline(cin, tem);
-	while (tem != "") {
-		data.push_back(tem);
-		getline(cin, tem);
-	}
-
-	for (int i = 0; i < data.size(); i++) {
-		string rs = s.process(data[i]);
-		cout << rs << endl;
-	}
-
-	system("pause");
-	return 0;
-}
 
 /*
 int main(int argc, char *argv[]) {
