cdf3ef8f2856d34d9920ec5342c8a31804a41a46
DavorWade <daviswade@sina.com>
2016-08-08 22:39:37 +0800

    Counting Islands II

--- HihoCoder/HihoCoder/CountingIslandsII.cpp
+++ HihoCoder/HihoCoder/CountingIslandsII.cpp
@@ -1,37 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <vector>
 #include <string.h>
-#include <iostream>
-
-//http://hihocoder.com/contest/hihointerview15/problems
 
 using namespace std;
 
-typedef pair<int, int> pii;
-typedef long long LL;
-
 const int MAX = 1010;
-int fa[MAX * MAX];
-int grid[MAX][MAX];
-int dx[] = { 0, 0, 1, -1 };
-int dy[] = { 1, -1, 0, 0 };
+int pre[MAX * MAX];
+int grip[MAX][MAX];
+int dx[] = { 0, 0, -1, 1 };
+int dy[] = { -1, 1, 0, 0 };
 
 void clear() {
-    memset(fa, -1, sizeof fa);
-    memset(grid, 0, sizeof grid);
+    memset(pre, -1, sizeof(pre));
+    memset(grip, 0, sizeof(grip));
 }
 
-int findset(int x) {
-    if (fa[x] == -1) {
-        return fa[x] = x;
+int findPre(int x) {
+    if (pre[x] == -1) {
+        return pre[x] = x;
     }
-    return x == fa[x] ? x : fa[x] = findset(fa[x]);
+
+    return pre[x] == x ? pre[x] : findPre(pre[x]);
 }
 
-void unionset(int x, int y) {
-    int fx = findset(x), fy = findset(y);
+void unionSet(int x, int y) {
+    int fx = findPre(x);
+    int fy = findPre(y);
+
     if (fx != fy) {
-        fa[fx] = fy;
+        pre[fx] = fy;
     }
 }
 
@@ -39,25 +37,32 @@ void unionset(int x, int y) {
 int main() {
     clear();
     int cnt = 0;
-    int N; cin >> N;
-    for (int i = 0; i < N; i++) {
-        int x, y; cin >> x >> y;
-        grid[x][y] = 1;
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+
         cnt++;
+        grip[x][y] = 1;
+
         for (int j = 0; j < 4; j++) {
             int nx = x + dx[j];
             int ny = y + dy[j];
-            if (nx >= 0 && ny >= 0 && nx < 1000 && ny < 1000) {
-                if (grid[nx][ny] && findset(nx * 1000 + ny) != findset(x * 1000 + y)) {
+
+            if (0 <= nx && nx < 1000 && 0 <= ny && ny < 1000) {
+                if (grip[nx][ny] == 1 && findPre(nx * 1000 + ny) != findPre(x * 1000 + y)) {
                     cnt--;
-                    unionset(nx * 1000 + ny, x * 1000 + y);
+                    unionSet(nx * 1000 + ny, x * 1000 + y);
                 }
             }
         }
-        cout << cnt << endl;
+
+        printf("%d\n", cnt);
     }
 
-    system("pause");
+    //system("pause");
     return 0;
 }
 */
