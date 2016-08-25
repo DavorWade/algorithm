55b7913704d0c8ffc488836e02f3650b2bf77010
DavorWade <hxy_mail@163.com>
2016-08-25 23:31:42 +0800

    Palindrome Partitioning

---
+++ LintCode/LintCode/RestoreIPAddresses.cpp
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+    * @param s the IP string
+    * @return All possible valid IP addresses
+    */
+    vector<string> restoreIpAddresses(string& s) {
+        // Write your code here
+
+    }
+};
