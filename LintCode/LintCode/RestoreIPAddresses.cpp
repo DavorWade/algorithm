20e0e23ad7ab46669ad8502e9fa23c3f5633307b
DavorWade <hxy_mail@163.com>
2016-08-26 23:11:15 +0800

    Restore IP Addresses

--- LintCode/LintCode/RestoreIPAddresses.cpp
+++ LintCode/LintCode/RestoreIPAddresses.cpp
@@ -13,6 +13,46 @@ public:
     */
     vector<string> restoreIpAddresses(string& s) {
         // Write your code here
+        vector<string> res;
+        getRes(s, "", res, 4);
+        return res;
+    }
 
+    bool valid(string s) {
+        if (s.size() == 3 && (atoi(s.c_str())>255 || atoi(s.c_str()) == 0)) {
+            return false;
+        }
+        if (s.size() == 3 && s[0] == '0') {
+            return false;
+        }
+        if (s.size() == 2 && atoi(s.c_str()) == 0) {
+            return false;
+        }
+        if (s.size() == 2 && s[0] == '0') {
+            return false;
+        }
+
+        return true;
+    }
+
+    void getRes(string s, string r, vector<string> &res, int k) {
+        if (k == 0) {
+            if (s.empty()) {
+                res.push_back(r);
+            }
+            return;
+        }
+        else {
+            for (int i = 1; i <= 3; i++) {
+                if (s.size() >= i && valid(s.substr(0, i))) {
+                    if (k == 1) {
+                        getRes(s.substr(i), r + s.substr(0, i), res, k - 1);
+                    }
+                    else {
+                        getRes(s.substr(i), r + s.substr(0, i) + ".", res, k - 1);
+                    }
+                }
+            }
+        }
     }
 };
