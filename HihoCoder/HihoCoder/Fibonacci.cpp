244b61c65d1109fae4cad2ed5dca51bb0d0bf782
DavorWade <hxy_mail@163.com>
2016-09-09 19:25:51 +0800

    Fibonacci

--- HihoCoder/HihoCoder/Fibonacci.cpp
+++ HihoCoder/HihoCoder/Fibonacci.cpp
@@ -1,11 +1,48 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include<algorithm>
 #include <vector>
 
 using namespace std;
 
 //http://hihocoder.com/contest/hiho113/problem/1
 
+long long dp[100], m, n, a[1000009], f[100], ans = 0;
+
+int main() {
+    f[1] = 1;
+    f[2] = 1;
+    m = 2;
+
+    while (f[m]<100000) {
+        f[m + 1] = f[m] + f[m - 1];
+        m++;
+    }
+    scanf("%lld", &n);
+
+    for (int i = 0; i < n; i++) scanf("%lld", &a[i]);
+    for (int i = 0;i < n; i++) {
+        int it = lower_bound(f + 1, f + m + 1, a[i]) - f;
+        if (it >= 3 && it <= m&&f[it] == a[i]) {
+            dp[it] += dp[it - 1];
+            ans += dp[it - 1];
+            ans %= 1000000007;
+            dp[it] %= 1000000007;
+        }
+        else if (1 == a[i]) {
+            dp[2] += dp[1];
+            dp[1]++;
+            ans += dp[1];
+        }
+    }
+    printf("%lld\n", ans);
+
+    return 0;
+}
+
+
+/*
+// DFS, Time Limited Error.
 void dfs(vector<int> &dt, int n, vector<int> rs, int &totalCount) {
     if (n == dt.size() && !rs.empty()) {
         totalCount++;
@@ -34,7 +71,7 @@ void dfs(vector<int> &dt, int n, vector<int> rs, int &totalCount) {
     }
 }
 
-/*
+
 int main() {
     vector<int> rs;
     vector<int> dt;
