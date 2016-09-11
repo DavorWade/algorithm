d79c0440b61320a601e8c52fc566bd39fbe84091
DavorWade <hxy_mail@163.com>
2016-09-11 11:58:02 +0800

    Compare Strings

--- LintCode/LintCode/CompareStrings.cpp
+++ LintCode/LintCode/CompareStrings.cpp
@@ -39,3 +39,35 @@ public:
         return rs;
     }
 };
+
+// another solution
+class Solution {
+public:
+    /**
+    * @param A: A string includes Upper Case letters
+    * @param B: A string includes Upper Case letter
+    * @return:  if string A contains all of the characters in B return true
+    *           else return false
+    * @date: 2016-9-10
+    */
+    bool compareStrings(string A, string B) {
+        // write your code here
+        if (A.size() < B.size()) return false;
+        if (B.size() == 0) return true;
+
+        vector<int> a(26, 0);
+        vector<int> b(26, 0);
+
+        for (int i = 0; i < A.size(); i++) {
+            a[A[i] - 'A']++;
+        }
+
+        for (int i = 0; i < B.size(); i++) {
+            b[B[i] - 'A']++;
+            if (a[B[i] - 'A'] < b[B[i] - 'A']) return false;
+        }
+
+        return true;
+    }
+};
+
