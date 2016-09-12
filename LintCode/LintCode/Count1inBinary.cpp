92e4fd07f9716368ba6283a99f897b752298483f
DavorWade <hxy_mail@163.com>
2016-09-12 09:47:48 +0800

    Count 1 in Binary

---
+++ LintCode/LintCode/Count1inBinary.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+    * @param num: an integer
+    * @return: an integer, the number of ones in num
+    */
+    int countOnes(int num) {
+        // write your code here
+        int count = 0;
+        while (num) {
+            num &= (num - 1);
+            count++;
+        }
+        return count;
+    }
+};
