60b08e8bb69c412616268ff80a33b272001147eb
DavorWade <hxy_mail@163.com>
2016-09-19 23:50:53 +0800

    Word Ladder

---
+++ LintCode/LintCode/WordLadder.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+    * @param start, a string
+    * @param end, a string
+    * @param dict, a set of string
+    * @return an integer
+    */
+    int ladderLength(string start, string end, unordered_set<string> &dict) {
+        // write your code here
+
+    }
+};
