0e4699fcd465d7b7dd80bbdc33e3be966b0cf43d
DavorWade <hxy_mail@163.com>
2016-09-20 10:08:36 +0800

    Word Ladder

--- LintCode/LintCode/WordLadder.cpp
+++ LintCode/LintCode/WordLadder.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include <queue>
 #include <unordered_set>
 
 using namespace std;
@@ -15,6 +16,41 @@ public:
     */
     int ladderLength(string start, string end, unordered_set<string> &dict) {
         // write your code here
+        if (start == end) {
+            return 1;
+        }
+        int n = start.size();
+        if (n < 1 || n != end.size()) {
+            return 0;
+        }
 
+        queue<string> Q;
+        Q.push(start);
+        dict.erase(start);
+        int length = 2;
+
+        while (!Q.empty()) {
+            int size = Q.size();
+            for (int i = 0; i < size; i++) {
+                string word = Q.front(); Q.pop();
+                for (int i = 0; i < n; i++) {
+                    char oldChar = word[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == oldChar) continue;
+                        word[i] = c;
+                        if (word == end) {
+                            return length;
+                        }
+                        if (dict.find(word) != dict.end()) {
+                            Q.push(word);
+                            dict.erase(word);
+                        }
+                    }
+                    word[i] = oldChar;
+                }
+            }
+            length++;
+        }
+        return 0;
     }
 };
