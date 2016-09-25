38fbc72760ef713945aa2dae5fcc5aae0870ecde
DavorWade <hxy_mail@163.com>
2016-09-25 15:53:26 +0800

    N-Queens

--- LintCode/LintCode/N-Queens.cpp
+++ LintCode/LintCode/N-Queens.cpp
@@ -16,6 +16,44 @@ public:
     */
     vector<vector<string> > solveNQueens(int n) {
         // write your code here
+        vector<vector<string> > allSol;
+        vector<string> sol;
+        vector<int> col;
 
+        solveNQ(n, 0, col, sol, allSol);
+
+        return allSol;
+    }
+
+    void solveNQ(int rows, int currRow, vector<int> &col, vector<string> &sol, vector<vector<string> > &allSol) {
+        if (rows == currRow) {
+            allSol.push_back(sol);
+            return;
+        }
+
+        for (int i = 0; i < rows; i++) {
+            if (isValid(currRow, i, col)) {
+                string s(rows, '.');
+                s[i] = 'Q';
+
+                sol.push_back(s);
+                allSol.push_back(sol);
+                col.push_back(i);
+
+                solveNQ(rows, currRow + 1, col, sol, allSol);
+                sol.pop_back();
+                col.pop_back();
+            }
+        }
+    }
+
+    bool isValid(int currRow, int currCol, vector<int> &col) {
+        if (currRow < col.size()) return false;
+
+        for (int i = 0; i < col.size(); i++) {
+            if (currCol == col[i] || abs(currRow - i) == abs(currCol - col[i])) return false;
+        }
+
+        return true;
     }
 };
