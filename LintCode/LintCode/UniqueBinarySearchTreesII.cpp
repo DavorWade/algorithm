1e3110fe6d8fa701315f08107de2d404381fedfd
DavorWade <hxy_mail@163.com>
2016-10-07 21:41:21 +0800

    Unique Binary Search Trees II

--- LintCode/LintCode/UniqueBinarySearchTreesII.cpp
+++ LintCode/LintCode/UniqueBinarySearchTreesII.cpp
@@ -5,4 +5,48 @@
 
 using namespace std;
 
+class TreeNode {
+public:
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int val) {
+        this->val = val;
+        this->left = this->right = NULL;
+    }
+};
 
+class Solution {
+public:
+    /**
+    * @paramn n: An integer
+    * @return: A list of root
+    */
+    vector<TreeNode *> generateTrees(int n) {
+        // write your code here
+        return dfs(1, n);
+    }
+
+    vector<TreeNode *>  dfs(int min, int max) {
+        vector<TreeNode *> rs;
+        if (min > max) {
+            rs.push_back(nullptr);
+            return rs;
+        }
+
+        for (int i = min; i <= max; i++) {
+            vector<TreeNode *> left = dfs(min, i - 1);
+            vector<TreeNode *> right = dfs(i + 1, max);
+
+            for (int j = 0; j < left.size(); j++) {
+                for (int k = 0; k < right.size(); k++) {
+                    TreeNode *root = new TreeNode(i);
+                    root->left = left[j];
+                    root->right = right[k];
+                    rs.push_back(root);
+                }
+            }
+        }
+
+        return rs;
+    }
+};
