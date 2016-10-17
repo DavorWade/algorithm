d3b169d27064465dfa4143384f88b7c14e2119b8
DavorWade <hxy_mail@163.com>
2016-10-17 12:05:49 +0800

    Composition

---
+++ HihoCoder/HihoCoder/Composition.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <limits.h>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N, M;
+    string composition;
+    vector<string> illegalPairs;
+
+    scanf("%d", &N);
+    cin >> composition;
+    scanf("%d", &M);
+    vector<int> dp(N + 1, INT_MAX);
+
+    for (int i = 0; i < M; i++) {
+        string tem;
+        cin >> tem;
+        illegalPairs.push_back(tem);
+
+        string temp;
+        temp.assign(tem.rbegin(), tem.rend());
+        illegalPairs.push_back(temp);
+    }
+
+    for (int j = 1; j < composition.size(); j++) {
+        string lastTwoChars = composition.substr(j - 1, 2);
+        if (find(illegalPairs.begin(), illegalPairs.end(), lastTwoChars) == illegalPairs.end()) {
+            dp[j + 1] = dp[j];
+        }
+        else {
+            dp[j + 1] = min(dp[j], dp[j - 1] + 1);
+        }
+    }
+
+    printf("%d\n", dp[N]);
+    return 0;
+}
