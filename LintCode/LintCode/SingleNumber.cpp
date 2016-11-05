73482f78e28ae7eae39c00b3c18f97a39fb8fee2
DavorWade <hxy_mail@163.com>
2016-11-05 10:24:05 +0800

    Single Number

--- LintCode/LintCode/SingleNumber.cpp
+++ LintCode/LintCode/SingleNumber.cpp
@@ -24,3 +24,21 @@ public:
     }
 };
 
+// bit operation
+class Solution {
+public:
+    /**
+    * @param A: Array of integers.
+    * return: The single number.
+    */
+    int singleNumber(vector<int> &A) {
+        // write your code here
+        if (A.size() == 0) return 0;
+        int left = A[0];
+        for (int i = 1; i < A.size(); i++) {
+            left = left ^ A[i];
+        }
+
+        return left;
+    }
+};
