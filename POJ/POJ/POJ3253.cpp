4e5981dadd7e13e94ced547c057200d441623b49
DavorWade <hxy_mail@163.com>
2017-01-11 02:12:37 +0800

    POJ3253

--- POJ/POJ/POJ3253.cpp
+++ POJ/POJ/POJ3253.cpp
@@ -1,50 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <vector>
+#include <cstdio>
+#include <queue>
+#include <functional> // std::greater
 
 using namespace std;
 
-int N;
+typedef long long int lli;
 
-int solve(vector<int> v, int N) {
-    long long rs = 0;
-
-    while (N > 1) {
-        int min1 = 0, min2 = 1;
-        if (v[min1] > v[min2]) swap(min1, min2);
-        for (int i = 2; i < N; i++) {
-            if (v[i] < v[min1]) {
-                min2 = min1;
-                min1 = i;
-            }
-            else if (v[i] < v[min2]) {
-                min2 = i;
-            }
-        }
-
-        int sum = v[min1] + v[min2];
-        rs += sum;
-
-        if (min1 == N - 1) swap(min1, min2);
-        v[min1] = sum;
-        v[min2] = v[N - 1];
-        N--;
-    }
-
-    return rs;
-}
+//https://programming-study-notes.blogspot.com/2014/03/poj-3253-fence-repair.html
 
 int main() {
-    scanf("%d", &N);
-    vector<int> v;
-    for (int i = 0; i < N; i++) {
-        int tem;
-        scanf("%d", &tem);
-        v.push_back(tem);
-    }
+    lli N, L;
+    while (scanf("%lld", &N) != EOF) {
+        priority_queue<lli, vector<lli>, greater<lli> > PQ;
 
-    int rs = solve(v, N);
-    printf("%ll\n", rs);
+        for (lli i = 0; i < N; ++i) {
+            scanf("%lld", &L);
+            PQ.push(L);
+        }
+
+        lli sum = 0;
+        while (PQ.size() > 1) {
+            lli tmp = PQ.top();
+            PQ.pop();
+            tmp += PQ.top();
+            PQ.pop();
+
+            sum += tmp;
+            PQ.push(tmp);
+        }
+        printf("%lld\n", sum);
+    }
 
     system("pause");
     return 0;
