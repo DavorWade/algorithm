b88aeffec0fd7395b0412c49f6b72b03fd02c90c
DavorWade <hxy_mail@163.com>
2017-01-20 15:06:33 +0800

    Trailing Zeros

---
+++ LintCode/LintCode/TrailingZeros.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    // param n : description of n
+    // return: description of return 
+    long long trailingZeros(long long n) {
+        long long sum = 0;
+        while (n != 0) {
+            sum += n / 5;
+            n /= 5;
+        }
+        return sum;
+    }
+};
